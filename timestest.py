9b73df72855189569b5778851640471538d23bf1
Jacob Schmitz <jake.schmitz101@gmail.com>
2014-06-21 17:27:25 -0700

    currently pulling most viewed articles for the month

--- timestest.py
+++ timestest.py
@@ -20,23 +20,45 @@
 # that would be particularly interesting.
 #
 
+from __future__ import print_function
 import os
 import time
+import requests
+import json
 
 class MostPopularInterface:
-  output_dir = ''
+  todays_file = "./data/" + time.strftime("%d_%m_%Y") + ".csv"
+  output_dir = open(todays_file, "w+")
+  calls_today = 0
+  today_began = time
   def __init__(self):
-    output_dir = './data/' + time.strftime("%d_%m_%Y") + '.csv'
+    calls_today = 0
+    today_began = time
+
+  def __del__(self):
+    self.output_dir.close()
 
   def popular_query_url(self, resourcetype="mostviewed", sections="all-sections", timeperiod=30, offset=0):
-    return 'http://api.nytimes.com/svc/mostpopular/v2/' + resourcetype + "/" + sections + "/" + str(timeperiod) + "?" + os.environ['MOST_POPULAR_KEY'] 
+    return 'http://api.nytimes.com/svc/mostpopular/v2/' + resourcetype + "/" + sections + "/" + str(timeperiod) + "?api-key=" + os.environ['MOST_POPULAR_KEY'] 
 
   def section_list_url(self, resourcetype="mostviewed"):
-    return 'http://api.nytimes.com/svc/mostpopular/v2/' + resourcetype + '/section-list?' + os.environ['MOST_POPULAR_KEY']
+    return 'http://api.nytimes.com/svc/mostpopular/v2/' + resourcetype + '/sections-list?api-key=' + os.environ['MOST_POPULAR_KEY']
+
+  def make_request(self, resourcetype="mostviewed", sections="all-sections", timeperiod=30, offset=0):
+    r = requests.get(self.popular_query_url(resourcetype, sections, timeperiod, offset))
+    return json.loads(r.text)['results']
+
+  def get_sections(self, resourcetype="mostviewed", sections="all-sections", timeperiod=30, offset=0):
+    r = requests.get(self.section_list_url(resourcetype))
+    return json.loads(r.text)['results'] 
+
+  def top_articles(self, resourcetype="mostviewed", sections="all-sections", timeperiod=30, offset=0):
+    for article in self.make_request(resourcetype, sections, timeperiod, offset):
+      self.output_dir.write('Article ID: ' + str(article['id']) + ', ')
+      self.output_dir.write('Title: '+ article['title'].encode('utf-8') + "\n")
 
 x = MostPopularInterface()
 
-print x.popular_query_url()
-print x.section_list_url()
+x.top_articles()
 
 
